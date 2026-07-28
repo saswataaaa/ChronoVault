df6175e69dd0bfb28b72ebd2338ccc6a369ff8f0
saswataaaa <saswatatripathy17@gmail.com>
2026-07-28 23:46:47 +0530

    feat: implement user login with JWT authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -58,8 +58,56 @@ export const register = async (req, res) => {
 };
 
 // Login (We'll build this next)
+// Login User
 export const login = async (req, res) => {
-  res.json({
-    message: "Login API Working",
-  });
+  try {
+    const { email, password } = req.body;
+
+    // Check missing fields
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Please fill all fields",
+      });
+    }
+
+    // Find user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    // Generate JWT
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.status(200).json({
+      message: "Login Successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
