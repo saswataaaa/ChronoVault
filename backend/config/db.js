9a86a3c3980f219e2a4408b4903f0109194b6d14
saswataaaa <saswatatripathy17@gmail.com>
2026-07-28 23:20:52 +0530

    feat: connect backend to MongoDB Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    console.log(process.env.MONGO_URI);
 
-    console.log("✅ MongoDB Connected");
+const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("❌ MongoDB Connection Failed");
-
+    console.error(error.message);
     process.exit(1);
   }
 };
